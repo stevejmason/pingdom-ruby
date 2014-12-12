d437091a5b568dd70ec16aaf9934ac5032274be9
Kerr Marin Miller <kerr.miller@net-a-porter.com>
2014-12-12 18:20:19 +0000

    Ruby syntax

--- lib/pingdom/client.rb
+++ lib/pingdom/client.rb
@@ -23,7 +23,7 @@ module Pingdom
 
         builder.basic_auth @options[:username], @options[:password]
         builder.headers["App-Key"] = @options[:key]
-	if(@options[:account_email])
+	if @options[:account_email]
           builder.headers["Account-Email"] = @options[:account_email]
       end
     end
